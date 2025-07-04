dc564d89b81c448c1c7a260cefd8e435a3dcb268
ElieOko <elieoko100@gmail.com>
2025-07-04 10:23:59 +0100

    fix:structure paiement components

---
+++ app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/payment/Paiement.kt
@@ -0,0 +1,18 @@
+package com.client.recouvrementapp.presentation.ui.pages.payment
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Paiement(){
+    PaiementBody()
+}
+
+@Composable
+fun PaiementBody(){
+
+}
+
+@Composable
+fun PaiementPreview(){
+    PaiementBody()
+}
