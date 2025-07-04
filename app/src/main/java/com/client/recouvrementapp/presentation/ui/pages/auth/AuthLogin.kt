c0c2f190d5863d156cac095d2bde21fbe0a01f0d
ElieOko <elieoko100@gmail.com>
2025-07-04 16:10:23 +0100

    fix:auth components

--- app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/auth/AuthLogin.kt
+++ app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/auth/AuthLogin.kt
@@ -1,7 +1,56 @@
 package com.client.recouvrementapp.presentation.ui.pages.auth
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.client.recouvrementapp.R
+import com.client.recouvrementapp.core.AsteriskPasswordVisualTransformation
+import com.client.recouvrementapp.presentation.components.animate.AnimatedBackgroundShapes
+import com.partners.hdfils_recolte.presentation.ui.components.Space
 
 @Composable
 fun AuthLogin(){
@@ -10,7 +59,157 @@ fun AuthLogin(){
 
 @Composable
 fun AuthLoginBody(){
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isAnimating by remember { mutableStateOf(false) }
+    var isActive by remember { mutableStateOf(true) }
+    var msg by remember { mutableStateOf("") }
+    var titleMsg by remember { mutableStateOf("Erreur") }
+    var isShow by remember { mutableStateOf(false) }
+    val maxLength = 10 // Longueur maximale
+    val isVisible = remember { mutableStateOf(false) }
+//    val rainbowColorsBrush = remember {
+//        Brush.sweepGradient(
+//            listOf(
+//                Color(0xFF380E81),
+//                Color(0xFF06230D),
+//                Color(0xFF2B4B4F),
+//                Color(0xFF17023D)
+//            )
+//        )
+//    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        AnimatedBackgroundShapes()
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            //0xFF3E4EBD
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                shape = RoundedCornerShape(20.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xFFFFFFFF)
+                )
+            ) {
+                Column(
+                    modifier = Modifier.padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Space(y=5)
+                    Image(
+                        painterResource(R.drawable.recouvrement),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(90.dp)
+                            .clip(CircleShape),
+                        contentScale = ContentScale.Crop)
+                    Space(y=10)
+                    Text(
+                        text = "Login",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                    Text(
+                        text = "Recouvrement management",
+                        fontSize = 14.sp,
+                        color = Color.Black.copy(alpha = 0.8f)
+                    )
 
+                    Spacer(modifier = Modifier.height(20.dp))
+                    OutlinedTextField(
+                        value = username,
+                        onValueChange = {
+                                username = it
+                        },
+                        label = { Text("Username") },
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.user),
+                                contentDescription = null,
+                                modifier = Modifier.size(25.dp),
+                                tint = Color.Black
+                            )
+                        },
+                        maxLines = 1,
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Black,
+                            unfocusedBorderColor = Color.Black,
+                            focusedLabelColor = Color.Black,
+                            unfocusedLabelColor = Color.Black,
+                            cursorColor = Color.Black,
+                            focusedLeadingIconColor = Color.Black,
+                            unfocusedLeadingIconColor = Color.Black
+                        ),
+                        textStyle = TextStyle(color = Color.Black),
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    Spacer(modifier = Modifier.height(6.dp))
+                    OutlinedTextField(
+                        value = password,
+                        onValueChange = { password = it },
+                        label = { Text("Password") },
+                        leadingIcon = {
+                            Icon(
+                                painter = painterResource(id = R.drawable.password),
+                                contentDescription = null,
+                                modifier = Modifier.size(25.dp),
+                                tint = Color.Black
+                            )
+                        },
+                        maxLines = 1,
+                        trailingIcon = {
+                            IconButton(onClick = {
+                                isVisible.value = !isVisible.value
+                            }) {
+                                Icon(painter = painterResource(if(isVisible.value) R.drawable.open else R.drawable.close), contentDescription = null, tint = Color.Black)
+                            }
+                        },
+                        visualTransformation = if(isVisible.value) VisualTransformation.None else AsteriskPasswordVisualTransformation(),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Black,
+                            unfocusedBorderColor = Color.Black,
+                            focusedLabelColor = Color.Black,
+                            unfocusedLabelColor = Color.Black,
+                            cursorColor = Color.Black,
+                            focusedLeadingIconColor = Color.Black,
+                            unfocusedLeadingIconColor = Color.Black
+                        ),
+                        textStyle = TextStyle(color = Color.Black),
+                        modifier = Modifier.fillMaxWidth(),
+                        shape = RoundedCornerShape(10.dp)
+                    )
+                    Space(y=20)
+                    Button(
+                        onClick = {},
+                        modifier = Modifier.fillMaxWidth(),
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor =  Color(0xFF15D77D),
+                            disabledContentColor = Color(0xFF080624),
+                            disabledContainerColor = Color(0xFF080624)
+                        )
+                    ) {
+                        Text(text = if(isActive) "Se connecter" else "Chargement...", fontSize = 16.sp, color = Color.White)
+                    }
+                    Space(y=20)
+                }
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
