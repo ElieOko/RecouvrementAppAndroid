4983a6b1d2677fbeb38f45574c51e7135369a776
ElieOko <elieoko100@gmail.com>
2025-07-04 10:23:38 +0100

    fix:structure recouvrement components

---
+++ app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/recouvrement/DetailRecouvrement.kt
@@ -0,0 +1,20 @@
+package com.client.recouvrementapp.presentation.ui.pages.recouvrement
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DetailRecouvrement(){
+    DetailRecouvrementBody()
+}
+
+@Composable
+fun DetailRecouvrementBody(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailRecouvrementPreview(){
+    DetailRecouvrementBody()
+}
---
+++ app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/recouvrement/HistoryRecouvrement.kt
@@ -0,0 +1,20 @@
+package com.client.recouvrementapp.presentation.ui.pages.recouvrement
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HistoryRecouvrement(){
+    HistoryRecouvrementBody()
+}
+
+@Composable
+fun HistoryRecouvrementBody(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HistoryRecouvrementPreview(){
+    HistoryRecouvrementBody()
+}
