4d12ef6e35600f18b8dcc0f4f0ec8f19298def4b
ElieOko <elieoko100@gmail.com>
2025-07-04 10:23:16 +0100

    fix:structure home components

---
+++ app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/home/Home.kt
@@ -0,0 +1,20 @@
+package com.client.recouvrementapp.presentation.ui.pages.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Home(){
+    HomeBody()
+}
+
+@Composable
+fun HomeBody(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomePreview(){
+    HomeBody()
+}
