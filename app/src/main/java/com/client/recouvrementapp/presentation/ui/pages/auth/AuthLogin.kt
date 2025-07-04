b82c7b0fd4f03e0ae5028fc76e8d5b7fa9dbdc67
ElieOko <elieoko100@gmail.com>
2025-07-04 10:16:05 +0100

    fix:structure auth login components

---
+++ app/src/main/java/com/client/recouvrementapp/presentation/ui/pages/auth/AuthLogin.kt
@@ -0,0 +1,20 @@
+package com.client.recouvrementapp.presentation.ui.pages.auth
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AuthLogin(){
+    AuthLoginBody()
+}
+
+@Composable
+fun AuthLoginBody(){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AuthLoginPreview(){
+    AuthLoginBody()
+}
