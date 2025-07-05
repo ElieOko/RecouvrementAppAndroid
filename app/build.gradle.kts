760417037dd427f6aa1a9377c62775bae5e12982
ElieOko <elieoko100@gmail.com>
2025-07-05 13:57:36 +0100

    fix:new dependancies navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation(libs.androidx.material3)
     //constraint-layout
     implementation(libs.androidx.constraintlayout.compose)
+    //navigation
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
