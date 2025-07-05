cd7cb9ff89478075c74f15b2779eef7126fb9dad
ElieOko <elieoko100@gmail.com>
2025-07-05 13:58:05 +0100

    fix:review structure

--- app/src/main/java/com/client/recouvrementapp/MainActivity.kt
+++ app/src/main/java/com/client/recouvrementapp/MainActivity.kt
@@ -7,16 +7,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.client.recouvrementapp.domain.route.Navigation
 import com.client.recouvrementapp.presentation.ui.theme.RecouvrementAppTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var navHostController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            navHostController = rememberNavController()
             RecouvrementAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                    Navigation(navHostController,innerPadding)
                 }
             }
         }
