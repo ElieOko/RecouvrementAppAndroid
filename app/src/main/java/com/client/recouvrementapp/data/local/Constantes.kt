2f6f44e8009677d3fb218e23f189662815603f38
ElieOko <elieoko100@gmail.com>
2025-07-07 18:10:44 +0100

    feat:data remote ktorclient android and utils data

---
+++ app/src/main/java/com/client/recouvrementapp/data/local/Constantes.kt
@@ -0,0 +1,12 @@
+package com.client.recouvrementapp.data.local
+
+class Constantes {
+    companion object {
+        const val BASE_URL = "https://dummyjson.com/"
+        const val HOST_DEV = "192.168.1.84:8000/api"
+        const val HOST_PROD = "custom.cadeauparfait1.com/api"
+        const val IS_PROD = true
+        const val TokenLocal = ""
+        const val API_INTERNET_MESSAGE="No Internet Connection"
+    }
+}
---
+++ app/src/main/java/com/client/recouvrementapp/data/remote/KtorClientAndroid.kt
@@ -0,0 +1,89 @@
+package com.client.recouvrementapp.data.remote
+
+import android.util.Log
+import com.client.recouvrementapp.data.local.Constantes.Companion.HOST_DEV
+import com.client.recouvrementapp.data.local.Constantes.Companion.HOST_PROD
+import com.client.recouvrementapp.data.local.Constantes.Companion.IS_PROD
+import com.client.recouvrementapp.data.local.Constantes.Companion.TokenLocal
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.observer.ResponseObserver
+import io.ktor.client.request.accept
+import io.ktor.client.request.get
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.HttpResponse
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.URLProtocol
+import io.ktor.http.contentType
+import io.ktor.http.encodedPath
+import io.ktor.http.headers
+import kotlinx.serialization.json.Json
+
+private const val NETWORK_TIME_OUT = 6_000L
+val httpClientAndroid = HttpClient(Android) {
+    install(ContentNegotiation) {
+        json(
+            Json {
+                prettyPrint = true
+                isLenient = true
+                useAlternativeNames = true
+                ignoreUnknownKeys = true
+                encodeDefaults = false
+            }
+        )
+    }
+    install(HttpTimeout) {
+        requestTimeoutMillis = NETWORK_TIME_OUT
+        connectTimeoutMillis = NETWORK_TIME_OUT
+        socketTimeoutMillis = NETWORK_TIME_OUT
+    }
+    install(ResponseObserver) {
+        onResponse { response ->
+            Log.d("HTTP status:", "${response.status.value}")
+        }
+    }
+    install(DefaultRequest) {
+        header(HttpHeaders.ContentType, ContentType.Application.Json)
+    }
+    defaultRequest {
+        contentType(ContentType.Application.Json)
+        accept(ContentType.Application.Json)
+    }
+}
+class KtorClientAndroid {
+    suspend fun postData(route: String, data: Any): HttpResponse {
+        return httpClientAndroid.post {
+            url {
+                protocol = if (IS_PROD) URLProtocol.HTTPS else URLProtocol.HTTP
+                host = if (IS_PROD) HOST_PROD else HOST_DEV
+                encodedPath = route
+            }
+            headers {
+                append(HttpHeaders.Authorization, TokenLocal)
+                append(HttpHeaders.ContentType, "application/json") // Set Content-Type
+            }
+            setBody(data) // Serialize the body
+        }
+    }
+    suspend fun getData(route: String): HttpResponse {
+        return httpClientAndroid.get {
+            url {
+                protocol = if (IS_PROD) URLProtocol.HTTPS else URLProtocol.HTTP
+                host = if (IS_PROD) HOST_PROD else HOST_DEV
+                encodedPath = route
+            }
+            headers {
+                append(HttpHeaders.Authorization, TokenLocal)
+            }
+        }
+    }
+}
+
