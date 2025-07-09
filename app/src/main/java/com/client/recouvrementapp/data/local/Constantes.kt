60f50844de5be4ba9629c198aee4cf0a307379e4
ElieOko <elieoko100@gmail.com>
2025-07-09 14:56:31 +0100

    fix:new constantes

--- app/src/main/java/com/client/recouvrementapp/data/local/Constantes.kt
+++ app/src/main/java/com/client/recouvrementapp/data/local/Constantes.kt
@@ -1,5 +1,9 @@
 package com.client.recouvrementapp.data.local
 
+import android.icu.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
 class Constantes {
     companion object {
         const val BASE_URL = "https://dummyjson.com/"
@@ -7,6 +11,14 @@ class Constantes {
         const val HOST_PROD = "custom.cadeauparfait1.com/api"
         const val IS_PROD = true
         const val TokenLocal = ""
+        const val subscriptionId = 10
+        const val cotisationOrdinaireId = 12
+        const val cotisationSpesmId = 13
         const val API_INTERNET_MESSAGE="No Internet Connection"
+        fun convertMillisToDate(millis: Long): String {
+            val formatter = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+            return formatter.format(Date(millis))
+        }
     }
+
 }
