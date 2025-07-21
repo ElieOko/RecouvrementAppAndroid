99395a8e74082592622560c07f729147d2e4e8cc
ElieOko <elieoko100@gmail.com>
2025-07-21 13:58:44 +0100

    feat structure database room

---
+++ app/src/main/java/com/client/recouvrementapp/data/room/RoomDatabase.kt
@@ -0,0 +1,9 @@
+package com.client.recouvrementapp.data.room
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [], version = 1, exportSchema = false)
+abstract class UserRoomDatabase : RoomDatabase() {
+
+}
