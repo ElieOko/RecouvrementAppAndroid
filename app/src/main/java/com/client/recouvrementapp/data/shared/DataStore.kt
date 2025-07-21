c0e5933d2cc776e192c787ae18eecf9396c54d99
ElieOko <elieoko100@gmail.com>
2025-07-21 13:02:46 +0100

    fix:datastore review structure store user_profile and delete all keys

--- app/src/main/java/com/client/recouvrementapp/data/shared/DataStore.kt
+++ app/src/main/java/com/client/recouvrementapp/data/shared/DataStore.kt
@@ -6,6 +6,8 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.client.recouvrementapp.domain.model.user.ProfilUser
+import com.client.recouvrementapp.domain.model.user.User
 import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
@@ -25,6 +27,24 @@ class StoreData(private val context: Context) {
     suspend fun setBluetoothPrinter(device : String) {
         context.dataStore.edit { settings ->
             settings[deviceBluetooth] = device
+        }
+    }
+
+    val getUser: Flow<ProfilUser> = context.dataStore.data
+        .map { preferences ->
+            val jsonString = preferences[user_key] ?: ""
+            gson.fromJson(jsonString, ProfilUser::class.java)
+        }
+
+    suspend fun saveUser(user: ProfilUser) {
+        context.dataStore.edit { preferences ->
+            val jsonString = gson.toJson(user)
+            preferences[user_key] = jsonString
+        }
+    }
+    suspend fun delete() {
+        context.dataStore.edit { preferences ->
+            preferences.clear()
         }
     }
 }
