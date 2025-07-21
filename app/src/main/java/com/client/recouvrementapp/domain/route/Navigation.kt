7e919c5a44e51206248b00bd956833f14a9b3798
ElieOko <elieoko100@gmail.com>
2025-07-21 10:58:23 +0100

    feat:new args navigation

--- app/src/main/java/com/client/recouvrementapp/domain/route/Navigation.kt
+++ app/src/main/java/com/client/recouvrementapp/domain/route/Navigation.kt
@@ -1,10 +1,14 @@
 package com.client.recouvrementapp.domain.route
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.client.recouvrementapp.domain.viewmodel.ParentViewModel
 import com.client.recouvrementapp.presentation.ui.pages.auth.AuthLogin
 import com.client.recouvrementapp.presentation.ui.pages.config.printer.PrinterConfig
 import com.client.recouvrementapp.presentation.ui.pages.home.Home
@@ -13,29 +17,35 @@ import com.client.recouvrementapp.presentation.ui.pages.payment.PaimentPrinter
 import com.client.recouvrementapp.presentation.ui.pages.recouvrement.DetailRecouvrement
 import com.client.recouvrementapp.presentation.ui.pages.recouvrement.HistoryRecouvrement
 
+@RequiresApi(Build.VERSION_CODES.TIRAMISU)
 @Composable
-fun Navigation(navC: NavHostController, innerPadding: PaddingValues){
+fun Navigation(
+    navC: NavHostController,
+    innerPadding: PaddingValues,
+    viewModelGlobal: ParentViewModel = viewModel(),
+    isConnected: Boolean
+){
     NavHost(navController = navC, startDestination = ScreenRoute.Login.name, route = "root") {
         composable(ScreenRoute.Login.name) {
-            AuthLogin(navC,onBackEvent={})
+            AuthLogin(navC,onBackEvent={},isConnected)
         }
         composable(ScreenRoute.Home.name) {
-            Home(navC)
+            Home(navC,isConnected)
         }
         composable(ScreenRoute.History.name) {
-            HistoryRecouvrement(navC,onBackEvent={navC.popBackStack()})
+            HistoryRecouvrement(navC,onBackEvent={navC.popBackStack()},isConnected)
         }
         composable(ScreenRoute.Detail.name) {
-            DetailRecouvrement(navC,onBackEvent={navC.popBackStack()})
+            DetailRecouvrement(navC,onBackEvent={navC.popBackStack()},isConnected)
         }
         composable(ScreenRoute.Payment.name) {
-            Paiement(navC,onBackEvent={navC.popBackStack()})
+            Paiement(navC,onBackEvent={navC.popBackStack()},isConnected)
         }
         composable(ScreenRoute.PrinterConfig.name) {
-            PrinterConfig(navC,onBackEvent={navC.popBackStack()})
+            PrinterConfig(navC, onBackEvent ={navC.popBackStack()}, viewModelGlobal.vmPrinterConfig,isConnected)
         }
         composable(ScreenRoute.PaymentPrinter.name) {
-            PaimentPrinter(navC,onBackEvent={navC.popBackStack()})
+            PaimentPrinter(navC,onBackEvent={navC.popBackStack()},isConnected)
         }
     }
 }
