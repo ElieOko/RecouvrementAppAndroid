d1cfd6c2d17ad2453e31c14167cf30dfa8bc34c6
ElieOko <elieoko100@gmail.com>
2025-07-22 17:26:36 +0100

    fix:change structure

--- app/src/main/java/com/client/recouvrementapp/domain/repository/room/RecouvrementRepository.kt
+++ app/src/main/java/com/client/recouvrementapp/domain/repository/room/RecouvrementRepository.kt
@@ -3,11 +3,14 @@ package com.client.recouvrementapp.domain.repository.room
 import androidx.annotation.WorkerThread
 import com.client.recouvrementapp.domain.interfaces.room.IRecouvrementDao
 import com.client.recouvrementapp.domain.model.room.RecouvrementModel
+import com.client.recouvrementapp.domain.model.room.RecouvrementWithRelations
 import kotlinx.coroutines.flow.Flow
 
 class RecouvrementRepository(private val recouvrementDao: IRecouvrementDao){
 
-    fun allRecouvrement(userId : Int) : Flow<List<RecouvrementModel>> = recouvrementDao.getAll(userId)
+    fun allRecouvrement(userId : Int) : Flow<List<RecouvrementWithRelations>> = recouvrementDao.getAll(userId)
+
+    fun allRecouvrementDay(dateCurrent: String, currencyId : Int) : Flow<List<RecouvrementWithRelations>> = recouvrementDao.getRecouvrementToDay(dateCurrent, currencyId)
 
     @Suppress("RedundantSuspendModifier")
     @WorkerThread
