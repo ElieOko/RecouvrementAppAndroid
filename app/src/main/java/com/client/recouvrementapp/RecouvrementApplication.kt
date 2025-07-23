efdef07c5166b9da90057b6dae38d19bf7d97062
ElieOko <elieoko100@gmail.com>
2025-07-23 11:07:04 +0100

    feat:configuration application

---
+++ app/src/main/java/com/client/recouvrementapp/RecouvrementApplication.kt
@@ -0,0 +1,24 @@
+package com.client.recouvrementapp
+
+import android.app.Application
+import com.client.recouvrementapp.data.room.RecouvrementRoomDatabase
+import com.client.recouvrementapp.domain.repository.room.CurrencyRepository
+import com.client.recouvrementapp.domain.repository.room.PaymentMethodRepository
+import com.client.recouvrementapp.domain.repository.room.PeriodRepository
+import com.client.recouvrementapp.domain.repository.room.RecouvrementRepository
+import com.client.recouvrementapp.domain.repository.room.TransactionTypeRepository
+import com.client.recouvrementapp.domain.repository.room.UserRepository
+
+class RecouvrementApplication : Application() {
+    val database by lazy { RecouvrementRoomDatabase.getDatabase(this) }
+    val userRepository by lazy { UserRepository(database.userDao()) }
+    val recouvrementRepository by lazy { RecouvrementRepository(database.recouvrementDao()) }
+    val paymentMethodRepository by lazy { PaymentMethodRepository(database.paymentMethodDao()) }
+    val currencyRepository by lazy { CurrencyRepository(database.currencyDao()) }
+    val transactionTypeRepository by lazy { TransactionTypeRepository(database.transactionTypeDao()) }
+    val periodRepository by lazy { PeriodRepository(database.periodDao()) }
+
+//    override fun onTrimMemory(level: Int) {
+//        super.onTrimMemory(level)
+//    }
+}
