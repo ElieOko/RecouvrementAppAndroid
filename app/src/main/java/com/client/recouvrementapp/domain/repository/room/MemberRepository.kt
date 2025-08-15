73a177a0db9c55bef6a78a34a5f2ed35c4ee3b80
ElieOko <elieoko100@gmail.com>
2025-08-15 12:13:30 +0100

    feat:repository member

--- app/src/main/java/com/client/recouvrementapp/domain/repository/room/MemberRepository.kt
+++ app/src/main/java/com/client/recouvrementapp/domain/repository/room/MemberRepository.kt
@@ -1,4 +1,20 @@
 package com.client.recouvrementapp.domain.repository.room
 
-class MembreRepository {
+import androidx.annotation.WorkerThread
+import com.client.recouvrementapp.domain.interfaces.room.IMemberDao
+import com.client.recouvrementapp.domain.model.room.MemberModel
+
+class MemberRepository(val dao: IMemberDao) {
+    @WorkerThread
+    fun allMember () : List<MemberModel> = dao.getAll()
+
+    @WorkerThread
+    suspend fun insert(memberModel: MemberModel) {
+        dao.insertAll(memberModel)
+    }
+
+    @WorkerThread
+    suspend fun update(memberModel: MemberModel) {
+        dao.updateAll(memberModel)
+    }
 }
