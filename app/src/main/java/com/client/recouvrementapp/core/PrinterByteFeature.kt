31f6f13d3245d1d54ac141fbee0efa8749ab0abb
ElieOko <elieoko100@gmail.com>
2025-08-15 11:53:41 +0100

    fix:core printer features commande

--- app/src/main/java/com/client/recouvrementapp/core/PrinterByteFeature.kt
+++ app/src/main/java/com/client/recouvrementapp/core/PrinterByteFeature.kt
@@ -1,6 +1,26 @@
 package com.client.recouvrementapp.core
 
+import kotlin.byteArrayOf
+
 object PrinterByteFeature {
-    val ESC      = 0x1B
-    val FormFeed = 0x0C
+    const val cmdEsc   : Byte   = 0x1B
+    const val tab   : Byte   = 0x09
+    const val FormFeed = 0x0C
+    val leftAlignemt = byteArrayOf(cmdEsc,0x61,0x00)
+    val centerAlignemt = byteArrayOf(cmdEsc,0x61,0x01)
+    val rightAlignemt = byteArrayOf(cmdEsc,0x61,0x02)
+    val italiqueOn = byteArrayOf(cmdEsc,0x34,0x01)
+    val italiqueOff = byteArrayOf(cmdEsc,0x34,0x00)
+    val boldOn = byteArrayOf(cmdEsc,0x45,0x00)
+    val boldOff = byteArrayOf(cmdEsc,0x45,0x01)
+    fun line(n : Byte = 0x02): ByteArray {
+        val line = byteArrayOf(cmdEsc,0x64, n)
+        return line
+    }
+    val title = "FJS"
+    val subTitle = "Reçu"
+    fun textAlignRow(label : String, valueText : String){
+        val first = label.toByteArray(charset("GBK"))
+        val second = valueText.toByteArray(charset("GBK"))
+    }
 }
